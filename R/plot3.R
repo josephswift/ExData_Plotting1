9cedb68e191f2e077dc1f72fd683846657977992
Joseph Swift <jwswift1@yahoo.com>
2020-07-03 14:20:18 -0400

    Change legend X scaling to fit long text for plot3 and plot4

--- R/plot3.R
+++ R/plot3.R
@@ -12,7 +12,9 @@ dataToAnalyze <- mutate(dataToAnalyze, dateTime = as.POSIXct(strptime(paste(data
 plot(dataToAnalyze$dateTime, dataToAnalyze$Sub_metering_1, type = "l", main = "", ylab = "Energy sub metering", xlab = "")
 points(dataToAnalyze$dateTime, dataToAnalyze$Sub_metering_2, type = "l", col = "red")
 points(dataToAnalyze$dateTime, dataToAnalyze$Sub_metering_3, type = "l", col = "blue")
-legend("topright", lty = 1, lwd = 1, pch = c(NA,NA,NA), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+#scale legend text to fit the labels
+legend("topright", lty = 1, lwd = 1, pch = c(NA,NA,NA), col = c("black", "red", "blue"), cex = .75, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.copy(device = png, file = "./figure/plot3.png")
 
--- R/plot4.R
+++ R/plot4.R
@@ -18,7 +18,7 @@ plot(dataToAnalyze$dateTime, dataToAnalyze$Voltage, type = "l", main = "", xlab=
 plot(dataToAnalyze$dateTime, dataToAnalyze$Sub_metering_1, type = "l", main = "", ylab = "Energy sub metering", xlab = "")
 points(dataToAnalyze$dateTime, dataToAnalyze$Sub_metering_2, type = "l", col = "red")
 points(dataToAnalyze$dateTime, dataToAnalyze$Sub_metering_3, type = "l", col = "blue")
-legend("topright", lty = 1, lwd = 1, pch = c(NA,NA,NA), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty = 1, lwd = 1, pch = c(NA,NA,NA), col = c("black", "red", "blue"), cex = .75, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 plot(dataToAnalyze$dateTime, dataToAnalyze$Global_reactive_power, type = "l", main = "", xlab="datetime", ylab = "Global_reactive_power")
 
